4bd5dcd318a2f822fa7e798c056da1479f85bd6a
Ikram khan <ikcode4599@gmail.com>
2024-07-14 00:19:26 +0500

    overflow-hidden

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import { buttonVariants } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="bg-slate-50">
+    <div className="bg-slate-50 overflow-hidden">
        <section>
         <MaxWidthWrapper className='pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52'>
           <div className='col-span-2 px-6 lg:px-0 lg:pt-4'>
