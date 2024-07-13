676cdb29da652accd6a881fd85ecf5b9948ef55e
Ikram khan <ikcode4599@gmail.com>
2024-07-13 22:54:00 +0500

    second

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,8 +8,7 @@ const Navbar = async () => {
   const { getUser } = getKindeServerSession()
   const user = await getUser()
 
-  const isAdmin = true;
-  // const isAdmin = user?.email === process.env.ADMIN_EMAIL
+  const isAdmin = user?.email === process.env.ADMIN_EMAIL
 
   return (
     <nav className='sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
