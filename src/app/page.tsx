5c05dc82637ce419fc73895053e9796407d4a14e
Ikram khan <ikcode4599@gmail.com>
2024-07-14 00:27:18 +0500

    small-img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -98,7 +98,7 @@ export default function Home() {
             <div className='relative md:max-w-xl'>
               <img
                 src='/your-image.png'
-                className='absolute w-40 lg:w-52 left-56 -top-20 select-none hidden sm:block lg:hidden xl:block'
+                className='absolute w-40 lg:w-28 left-48 -top-20 select-none hidden sm:block lg:hidden xl:block'
              alt="" />
               <img
                 src='/line.png'
